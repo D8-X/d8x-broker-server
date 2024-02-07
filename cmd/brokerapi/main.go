8fa449185390422f86f1af399360593d17236a57
Mantas <j.mantelijo@gmail.com>
2024-02-07 10:23:50 +0200

    add version injection

--- cmd/brokerapi/main.go
+++ cmd/brokerapi/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/D8-X/d8x-broker-server/src/svc"
+import (
+	"log/slog"
+
+	"github.com/D8-X/d8x-broker-server/src/svc"
+)
+
+// Injected via -ldflags -X
+var VERSION = "broker-api-development"
 
 func main() {
+	slog.Info("starting service",
+		slog.String("name", "broker-api"),
+		slog.String("version", VERSION),
+	)
 	svc.RunBroker()
 }
