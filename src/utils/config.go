c39d6e933bab42bafd7ccd2826ef00279f6f70cc
b1674927 <b1674927@protonmail.com>
2025-03-19 12:12:44 +0100

    undo

---
+++ src/utils/config.go
@@ -0,0 +1,63 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	d8x_config "github.com/D8-X/d8x-futures-go-sdk/config"
+)
+
+// load configuration json with deployment addresses: "config/chainConfig.json"
+func LoadChainConfig(configName string) (map[int64]ChainConfig, error) {
+	// Read the JSON file
+	data, err := os.ReadFile(configName)
+	if err != nil {
+		log.Fatal("Error reading JSON file:", err)
+		return nil, err
+	}
+	var configuration []ChainConfigFile
+	// Unmarshal the JSON data into the Configuration struct
+	err = json.Unmarshal(data, &configuration)
+	if err != nil {
+		log.Fatal("Error decoding JSON:", err)
+		return nil, err
+	}
+	config := make(map[int64]ChainConfig)
+	for k := 0; k < len(configuration); k++ {
+		if len(configuration[k].AllowedExecutors) == 0 {
+			// we have no executors whitelisted
+			continue
+		}
+		sdkConf, err := d8x_config.GetDefaultChainConfigFromId(configuration[k].ChainId)
+		if err != nil {
+			return nil, fmt.Errorf("unable to find sdk chain config for chain %d", configuration[k].ChainId)
+		}
+		config[configuration[k].ChainId] = ChainConfig{
+			ChainId:           configuration[k].ChainId,
+			Name:              configuration[k].Name,
+			AllowedExecutors:  configuration[k].AllowedExecutors,
+			MultiPayCtrctAddr: sdkConf.MultiPayAddr,
+		}
+	}
+	return config, nil
+}
+
+// load configuration json with deployment addresses: "config/rpcConfig.json"
+func LoadRpcConfig(configName string) ([]RpcConfig, error) {
+	// Read the JSON file
+	data, err := os.ReadFile(configName)
+	if err != nil {
+		log.Fatal("Error reading JSON file:", err)
+		return []RpcConfig{}, err
+	}
+	var configuration []RpcConfig
+	// Unmarshal the JSON data into the Configuration struct
+	err = json.Unmarshal(data, &configuration)
+	if err != nil {
+		log.Fatal("Error decoding JSON:", err)
+		return []RpcConfig{}, err
+	}
+	return configuration, nil
+}
